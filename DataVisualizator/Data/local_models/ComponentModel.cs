2ffff42dc5ede3dda8c1b1b39c4616bf4c62e9be
victor-meza-07 <victord.meza@gmail.com>
2020-07-12 14:35:47 -0500

    init

---
+++ DataVisualizator/Data/local_models/ComponentModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataVisualizator.Data.local_models
+{
+    public class ComponentModel
+    {
+    }
+}
